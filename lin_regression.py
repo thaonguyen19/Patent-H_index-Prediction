4d5c94508b5491a5b8dd80280f0a71b16dd57401
StephanieMallard <smallard@stanford.edu>
2017-12-06 14:06:48 -0800

    predicting on hindex instead

--- lin_regression.py
+++ lin_regression.py
@@ -10,12 +10,17 @@ Inputs are json files named by company
 from sklearn.model_selection import cross_val_predict
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+from sklearn.decomposition import PCA
 import os
 import json
 import numpy as np
 
 network_folder = '../data/networks/'
 num_features = 9
+#convert prediction to a ranking
+use_ranking = False
+#how many features to reduce to (set = to num_components for no pca)
+pca_components = 9
 
 def load_data(folder):
     x = []
@@ -30,7 +35,8 @@ def load_data(folder):
         #Load each company's JSON file
         with open(name, 'r') as fp:
             data = json.load(fp)
-        company_y = data.pop('forward_citation_count')
+        company_y = data.pop('hindex')
+        data.pop('forward_citation_count')        
         company_x = []
         for k in sorted(data):
             company_x.append(data[k])
@@ -49,11 +55,21 @@ def load_data(folder):
     company_names = [os.path.splitext(os.path.split(n)[1])[0] for n in company_names]
     return company_names, x, y
 
+def convert_to_rank(Y):
+    Y_sorted = sorted(Y)
+    Y_idx = [Y_sorted.index(y) for y in Y]
+    return Y_idx
+
 def main():
     company_names, X, Y = load_data(network_folder)
     lr = linear_model.LinearRegression()
+    pca = PCA(n_components=pca_components)
+    X = pca.fit_transform(X)
     #Run k-fold cross validation and prediction simultaneously
-    Y_pred = cross_val_predict(lr, X, Y, cv=10)
+    Y_pred = cross_val_predict(lr, X, Y, cv=8)
+    if use_ranking:
+        Y = convert_to_rank(Y)
+        Y_pred = convert_to_rank(Y_pred)
     for pred_pair in zip(Y, Y_pred):
         print "Actual: %s, Predicted: %s" %pred_pair
     print mean_squared_error(Y, Y_pred)
